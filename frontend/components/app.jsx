87e5c82d836272c25dd969db98b5a003c1379d9a
Alexander George <alexander97.george@gmail.com>
2020-03-03 10:47:06 -0800

    cleared landing page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,11 +4,11 @@ import { AuthRoute, ProtectedRoute } from '../utils/route_util';
 import SignupContainer from './session/signup_container';
 import LoginContainer from './session/login_container';
 import TopBarContainer from './top-bar/top_bar_container';
-import Home from './home';
+import Landing from './landing/landing';
 
 export default () => (
   <Switch>
-    <Route exact path="/" component={Home} />
+    <Route exact path="/" component={Landing} />
     <AuthRoute path="/signup" component={SignupContainer} />
     <AuthRoute path="/login" component={LoginContainer} />
   </Switch>
---
+++ frontend/components/landing/landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TopBarContainer from '../top-bar/top_bar_container';
+import './landing.css'
+
+function Landing() {
+  return (
+    <div className="landing">
+      <TopBarContainer />
+    </div>
+  )
+}
+
+export default Landing;
