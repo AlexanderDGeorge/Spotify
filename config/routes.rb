7e6e6ca99ecb7016531be28c082614e3630c0b7e
Alexander George <alexander97.george@gmail.com>
2020-03-05 12:38:50 -0800

    complete routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json } do
-    resource :session, only: [:new, :create, :destroy]
-    resources :users
+    resource :session, only: [:create, :destroy]
+    resources :users, only: [:create]
     resources :playlists, only: [:create, :index, :show, :destroy]
+    resources :playlist_songs, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
+    resources :songs, only: [:index, :show]
+    resources :artists, only: [:index, :show]
+    resources :albums, only: [:index, :show]
   end
 
   root to: 'root#root'
