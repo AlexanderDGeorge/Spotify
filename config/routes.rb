114b416004082dd08e5ddaf1f596c25c8134a464
Alexander George <alexander97.george@gmail.com>
2020-03-09 11:08:50 -0700

    push to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'root#root'
 
   namespace :api, defaults: { format: :json } do
     resource :session, only: [:create, :destroy]
@@ -11,7 +13,4 @@ Rails.application.routes.draw do
     resources :artists, only: [:index, :show]
     resources :albums, only: [:index, :show]
   end
-
-  root to: 'root#root'
-
 end
