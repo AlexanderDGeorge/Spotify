f41945f27b70a7cda0dbec133b1f3874f4bfb42e
Alexander George <alexander97.george@gmail.com>
2020-03-11 12:30:04 -0700

    implemented search

--- frontend/components/search/results.jsx
+++ frontend/components/search/results.jsx
@@ -1,54 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import { fetchSongs } from '../../actions/song_actions';
-import { fetchAlbums } from '../../actions/album_actions';
-import { fetchArtists } from '../../actions/artist_actions';
-import { fetchPlaylists } from '../../actions/playlist_actions';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function Results(props) {
 
-    let results = [];
+    console.log(props.search)
     const { artists, albums, playlists, songs } = props;
-
-    console.log(props);
+    const [albumResults, setAlbums] = useState([]);
+    const [artistResults, setArtists] = useState([]);
+    const [playlistResults, setPlaylists] = useState([]);
+    const [songResults, setSongs] = useState([]);
 
     useEffect(() => {
-        fetch();
         search();
-    }, []);
-
-    function fetch() {
-        props.fetchAlbums();
-        props.fetchArtists();
-        props.fetchPlaylists();
-        props.fetchSongs();
-    }
+    }, [props.search]);
 
     function search() {
         if (props.search) {
-            results.push(artists.filter(artist => artist.name.toLowerCase().includes(props.search.toLowerCase())));
-            results.push(albums.filter(album => album.name.toLowerCase().includes(props.search.toLowerCase())));
-            results.push(playlists.filter(playlist => playlist.name.toLowerCase().includes(props.search.toLowerCase())));
-            results.push(songs.filter(song => song.name.toLowerCase().includes(props.search.toLowerCase())));
+            // debugger
+            setAlbums(albums.filter(album => album.name.toLowerCase().includes(props.search.toLowerCase())));
+            setArtists(artists.filter(artist => artist.name.toLowerCase().includes(props.search.toLowerCase())));
+            setPlaylists(playlists.filter(playlist => playlist.name.toLowerCase().includes(props.search.toLowerCase())));
+            setSongs(songs.filter(song => song.name.toLowerCase().includes(props.search.toLowerCase())));
         } else {
-            results.push(artists.name);
-            results.push(albums.name);
-            results.push(playlists.name);
-            results.push(songs.name);
+            setAlbums(albums);
+            setArtists(artists);
+            setPlaylists(playlists);
+            setSongs(songs);
         }
     }
 
-    if (songs.length === 0 || albums.length === 0) return null;
-    else {
-        console.log(results);
-        return (
-            <div className="results">
-                { results.map(result => {
-                    return <div>{result.name}</div>
+    // console.log(albumResults, artistResults, playlistResults, songResults)
+
+    return (
+        <div className="results">
+            <h2>Albums</h2>
+            <div className="results-div">
+                {albumResults.map(album => {
+                    return (
+                        <Link to={"albums/" + album.id}>{album.name}</Link>
+                    )
                 })}
             </div>
-        )
-    }
+
+            <h2>Artists</h2>
+            <div className="results-div">
+                {artistResults.map(artist => {
+                    return (
+                        <Link to={"artists/" + artist.id}>{artist.name}</Link>
+                    )
+                })}
+            </div>
+
+            <h2>Playlists</h2>
+            <div className="results-div">
+                {playlistResults.map(playlist => {
+                    return (
+                        <Link to={"playlists/" + playlist.id}>{playlist.name}</Link>
+                    )
+                })}
+            </div>
+
+            <h2>Songs</h2>
+            <div className="results-div">
+                {songResults.map(song => {
+                    return (
+                        <Link to={"albums/" + song.artist_id}>{song.name}</Link>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 
 const mapState = state => ({
@@ -58,11 +80,4 @@ const mapState = state => ({
     songs: Object.values(state.entities.songs)
 })
 
-const mapDispatch = dispatch => ({
-    fetchAlbums: props => dispatch(fetchAlbums(props)),
-    fetchArtists: props => dispatch(fetchArtists(props)),
-    fetchPlaylists: props => dispatch(fetchPlaylists(props)),
-    fetchSongs: props => dispatch(fetchSongs(props)),
-})
-
-export default connect(mapState, mapDispatch)(Results);
+export default connect(mapState, undefined)(Results);
--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,30 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchSongs } from '../../actions/song_actions';
+import { fetchAlbums } from '../../actions/album_actions';
+import { fetchArtists } from '../../actions/artist_actions';
+import { fetchPlaylists } from '../../actions/playlist_actions';
 import Results from './results';
 import './search.css';
 
 function Search(props) {
 
     const [search, setSearch] = useState("");
+    const { artists, albums, playlists, songs } = props;
+
+    useEffect(() => {
+        props.fetchAlbums();
+        props.fetchArtists();
+        props.fetchPlaylists();
+        props.fetchSongs()
+    }, [])
 
     function handleSearch(e) {
         setSearch(e.target.value);
     }
 
-    return (
-        <div className="search">
-            <form onSubmit={handleSearch}>
-                <input 
-                    className="search-input"
-                    type="text"
-                    value={search}
-                    onChange={handleSearch}
-                    onBlur={handleSearch}
-                    placeholder="search"
-                />
-            </form>
-            <Results search={search}/>
-        </div>
-    )
+    if (artists.length && albums.length && playlists.length && songs.length) {
+        console.log(search)
+        return (
+            <div className="search">
+                <form onSubmit={handleSearch}>
+                    <input 
+                        className="search-input"
+                        type="text"
+                        value={search}
+                        onChange={handleSearch}
+                        onBlur={handleSearch}
+                        placeholder="search"
+                    />
+                </form>
+                <Results search={search}/>
+            </div>
+        )
+    } else {
+        return null;
+    }
 }
 
-export default Search;
+const mapState = state => ({
+    artists: Object.values(state.entities.artists),
+    albums: Object.values(state.entities.albums),
+    playlists: Object.values(state.entities.playlists),
+    songs: Object.values(state.entities.songs)
+})
+
+const mapDispatch = dispatch => ({
+    fetchAlbums: () => dispatch(fetchAlbums()),
+    fetchArtists: () => dispatch(fetchArtists()),
+    fetchPlaylists: () => dispatch(fetchPlaylists()),
+    fetchSongs: () => dispatch(fetchSongs()),
+})
+
+export default connect(mapState, mapDispatch)(Search);
