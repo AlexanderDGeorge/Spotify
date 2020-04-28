96104a661fc6a351c327289fa6c960262a7f9b31
Alexander George <alexander97.george@gmail.com>
2020-04-28 11:37:41 -0700

    add cover image on play

--- frontend/components/cover/cover.jsx
+++ frontend/components/cover/cover.jsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { fetchAlbums } from '../../actions/album_actions';
+import './cover.css';
 
 function Cover(props) {
-    const { songArray } = props.queue;
-    console.log(songArray);
-    return (
-        <div className='cover'>
 
-        </div>
-    )
+    const { albums } = props;
+    const { songArray } = props.queue;
+
+    useEffect(() => {
+        fetchAlbums();
+    }, [])
+
+    function fetchCover() {
+        let albumId = songArray[0].album_id;
+        let cover = albums[albumId].img_url;
+        return cover;
+    }
+    
+    if (songArray.length) {
+        return (
+            <div className='cover'>
+                <img src={fetchCover()} />
+            </div>
+        )
+    } else {
+        return (
+            <div className='cover'></div>
+        )
+    }
 }
 
 const mapState = state => ({
     queue: state.entities.queue,
+    albums: state.entities.albums,
+})
+
+const mapDispatch = dispatch => ({
+    fetchAlbums: () => dispatch(fetchAlbums()),
 })
 
 export default connect(mapState, null)(Cover);
