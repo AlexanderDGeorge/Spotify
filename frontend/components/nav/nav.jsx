7ae1e32d77ce477eacd987cb07a8aa41b395223b
Alexander George <alexander97.george@gmail.com>
2020-04-29 16:04:49 -0700

    condensing of components

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -1,17 +1,55 @@
-import React from 'react';
-import Spotlofi from '../spotlofi/spotlofi';
-import NavLinks from './nav-links';
-import PlaylistLinks from '../playlist/playlist-links';
+import React, { useState, useEffect } from 'react';
+import { FaHome, FaSearch, FaPlusSquare, FaHeart } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchPlaylists } from '../../actions/playlist_actions';
+
 import './nav.css'
 
-function Nav() {
+function Nav(props) {
+
+    const [active, setActive] = useState(null);
+
+    useEffect(() => {
+        props.fetchPlaylists();
+    }, []);
+
     return(
         <div className="nav">
-            <Spotlofi />
-            <NavLinks />
-            <PlaylistLinks />
+            <Link className={active === 0 ? "active" : ""} onClick={() => setActive(0)} to="/">
+                <FaHome />
+                Home
+            </Link>
+            <Link className={active === 1 ? "active" : ""} onClick={() => setActive(1)} to="/search">
+                <FaSearch />
+                Search
+            </Link>
+            <Link className={active === 2 ? "active" : ""} onClick={() => setActive(2)} to="/liked">
+                <FaHeart />
+                Liked Songs
+            </Link>
+            <Link className={active === 3 ? "active" : ""} onClick={() => setActive(3)} to="/playlists/new">
+                <FaPlusSquare />
+                Create Playlist
+            </Link>
+            <h4>PLAYLISTS</h4>
+            { props.playlists.map(playlist => {
+                return (
+                    <Link className="playlist-link" to={'/playlists/' + playlist.id} key={playlist.id}>
+                        { playlist.name }
+                    </Link>
+                )
+            })}
         </div>
     )
 }
 
-export default Nav;
+const mapState = state => ({
+    playlists: Object.values(state.entities.playlists)
+});
+
+const mapDispatch = dispatch => ({
+    fetchPlaylists: props => dispatch(fetchPlaylists(props))
+});
+
+export default connect(mapState, mapDispatch)(Nav);
